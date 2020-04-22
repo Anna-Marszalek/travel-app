99a9121e4e931d0c52050d734327c65e7476a937
aniaaa123 <aniaaa123@gmail.com>
2020-04-22 22:42:56 +0200

    WiP

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import './MainContainer.scss';
-import Sidebar from '../Sidebar/Sidebar';
+import React from "react";
+import "./MainContainer.scss";
+import Sidebar from "../Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 
 const MainContainer = (props) => {
-  
-    return (
-      <div className="main-container">
-        <Sidebar />
-        <div className="hotels">
-          <div className="tools" style={{width: '100%'}}>
-            <input type="button" value={props.sort ? 'A-Z' : 'Z-A'} onClick={props.switchSort} />
-          </div>
-          {props.data.map(hotel => {
-            return (
-              <div className="hotel">
-                <img src={hotel.image} alt={hotel.title} />
-                <div className="hotel__info">
-                  <span className="hotel-name">{hotel.title}</span>
-                  <p>{hotel.location}</p>
-                  <span className="s-price">{hotel.price}$</span>
-                </div>
-              </div>
-            );
-          })}
+  return (
+    <div className="main-container">
+      <label>
+      {/* <div className="currency">
+        <select onChange={props.convertPrice()} value={props.value}>
+          <option value="CHF">CHF</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="PLN">PLN</option>
+          <option defaultValue="USD">USD</option>
+        </select>
+      </div> */}
+      </label>
+      <Sidebar />
+      <div className="hotels">
+        <div className="tools" style={{ width: "100%" }}>
+          <input
+            type="button"
+            value={props.sort ? "A-Z" : "Z-A"}
+            onClick={props.switchSort}
+          />
         </div>
+        {props.data.map((hotel) => {
+          return (
+            <div className="hotel" key={hotel.id}>
+              <img src={hotel.image} alt={hotel.title} />
+              <div className="hotel__info">
+                <Link to={"hotel/" + hotel.id}>
+                  <span className="hotel-name">{hotel.title}</span>
+                </Link>
+                <p>{hotel.location}</p>
+                <span className="s-price">{hotel.price}$</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default MainContainer;
---
+++ src/utils/rates.js
@@ -0,0 +1,12 @@
+const rates = [
+    {
+     USD: 1,
+     EUR: 0.93,
+     PLN: 4.25,
+     GBP: 1.26,
+     CHF: 0.98
+    } 
+  ];
+  
+  export default rates;
+  
